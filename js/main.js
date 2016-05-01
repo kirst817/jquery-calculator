49a4c026281a22784b974e9b4be06dd6bbccbd30
Kirsten <kirstenwedde@gmail.com>
2016-05-01 10:48:40 -0600

    Refactored calc

--- js/main.js
+++ js/main.js
@@ -1,11 +1,52 @@
 $(document).ready(function() {
 
-  $('.buttons span').click(function() {
-	        var screenText = $('#screen').text();
-	        $('#screen').text(screenText + $(this).text());
-	    });
+var screenInput = [];
+var oper = "";
 
-  function resetScreen() {
-	$('#screen').text("");
-	}
+
+// // SET UP CLICK LISTERNER TO PICK UP VALUE CLICKED
+$('.buttons span').on('click', function(e){
+   var screenText = $('#screen').text();
+  $('#screen').text(screenText + $(e.target).text());
+
 });
+
+//CALCULATE FUNCTION
+function calculate(a , b){
+
+if (oper === "+"){
+  return $('#screen').text(a + b);
+}
+if (oper === "-"){
+  return $('#screen').text(a - b);
+}
+if (oper === "x"){
+  return $('#screen').text(a * b);
+}
+else {
+  return $('#screen').text(a / b);
+}
+
+}
+
+//ACCESSING NUM AND OPER AND CALLING CALC FUNCTION
+$('span').on('click', function(e) {
+  var screenInput = $('#screen').text().split(oper);
+  var a = parseInt(screenInput[0],10);
+  var b = parseInt(screenInput[1],10);
+  if($(e.target).hasClass('operator') && e.target.id !== ('calc')){
+      oper = $(e.target).text();
+    }
+  if(e.target.id === ('calc')){
+     return calculate(a, b);
+   }
+
+});
+
+//CLEAR SCREEN
+$('#cancel').on('click', function(){
+  $('#screen').text(" ");
+});
+
+
+ });
